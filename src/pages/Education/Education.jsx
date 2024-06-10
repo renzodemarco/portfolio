f0d7dff9f95e08f534003234519ed1fd51fdda7b
Renzo Demarco <renzo11demarco@gmail.com>
2024-06-10 15:03:09 -0300

    add grid education

--- src/pages/Education/Education.jsx
+++ src/pages/Education/Education.jsx
@@ -3,21 +3,38 @@ import './Education.css'
 const Education = () => {
   return (
     <div className='education-container'>
-      <div className="education-coder">
-        <h2>Coderhouse(2023)</h2>
+      <div className="education-coder education-div">
+        <h2>Coderhouse</h2>
         <ul>
           <li>Web Development: HTML, CSS, Javascript, React.js</li>
           <li>Backend programming: Node.js, Express.js</li>
-          <li>NoSQL Database Management</li>
           <li>Mobile Application Development: React Native</li>
+          <li>NoSQL Database Management</li>
         </ul>
       </div>
-      <div className="education-courses">
-        <h2>Other courses(2023)</h2>
+      <div className="education-courses education-div">
+        <h2>Other courses</h2>
         <ul>
+          <li>MySQL and Sequelize</li>
           <li>Git</li>
-          <li>MySQL</li>
+          <li>Docker</li>
           <li>Agile Methodologies</li>
+        </ul>
+      </div>
+      <div className="education-vet education-div">
+        <h2>Veterinary Medicine</h2>
+        <ul>
+          <li>Comprehensive training in Veterinary Medicine with experience in clinical practice, laboratory work, teaching, and research.</li>
+          <li>Developed strong problem-solving skills, attention to detail, and team leadership.</li>
+        </ul>
+      </div>
+      <div className="education-lang education-div">
+        <h2>Languages</h2>
+        <ul>
+          <li><span>Spanish</span> (native)</li>
+          <li><span>English</span> (advanced)
+            <p>IELTS Advanced English Certificate (2023)</p>
+          </li>
         </ul>
       </div>
     </div>
