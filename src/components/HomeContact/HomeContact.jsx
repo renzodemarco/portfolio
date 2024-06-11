a4355c38460d92edd3d3386d2ee5f88168ffee8e
Renzo Demarco <renzo11demarco@gmail.com>
2024-06-11 14:23:34 -0300

    add copy-mail button

--- src/components/HomeContact/HomeContact.jsx
+++ src/components/HomeContact/HomeContact.jsx
@@ -10,15 +10,15 @@ const HomeContact = () => {
       <h3>Contact me:</h3>
       <div className='home-contact-list'>
         <div className='home-contact-mail'>
-          <p>renzo11demarco@gmail.com</p>
           <button className='copy-button' onClick={() => alert('email copied')}>
+            <p>renzo11demarco@gmail.com</p>
             <CopyToClipboard text='renzo11demarco@gmail.com'>
               <TbCopy className='copy-icon' />
             </CopyToClipboard>
           </button>
         </div>
         <div className='home-contact-cv'>
-        <p>Download CV</p>
+          <p>Download CV</p>
         </div>
         <div className='home-contact-social'>
           <ButtonLink name='linkedin' link='https://linkedin.com/in/renzo-demarco' />
