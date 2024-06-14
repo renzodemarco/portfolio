fa2c5d44ca971c3cd5f4664c45a26ee3be54caf5
Renzo Demarco <renzo11demarco@gmail.com>
2024-06-14 11:47:13 -0300

    add education changes again

--- src/pages/Education/Education.jsx
+++ src/pages/Education/Education.jsx
@@ -3,7 +3,7 @@ import './Education.css'
 const Education = () => {
   return (
     <div className='education-container'>
-      <div className="education-coder education-div">
+      <div className="education-div education-coder">
         <h2>Coderhouse</h2>
         <ul>
           <li>Web Development: HTML, CSS, Javascript, React.js</li>
@@ -12,7 +12,7 @@ const Education = () => {
           <li>NoSQL Database Management</li>
         </ul>
       </div>
-      <div className="education-courses education-div">
+      <div className="education-div education-courses">
         <h2>Other courses</h2>
         <ul>
           <li>MySQL and Sequelize</li>
@@ -21,18 +21,18 @@ const Education = () => {
           <li>Agile Methodologies</li>
         </ul>
       </div>
-      <div className="education-vet education-div">
+      <div className="education-div education-vet">
         <h2>Veterinary Medicine</h2>
         <ul>
-          <li>Comprehensive training in Veterinary Medicine with experience in clinical practice, laboratory work, teaching, and research.</li>
-          <li>Developed strong problem-solving skills, attention to detail, and team leadership.</li>
+          <li>In 2020, I received my degree in Veterinary Medicine with a focus on clinical practice and laboratory research, teaching, and investigation</li>
+          <li>Developed strong problem-solving skills, attention to detail, and team leadership</li>
         </ul>
       </div>
-      <div className="education-lang education-div">
+      <div className="education-div education-lang">
         <h2>Languages</h2>
         <ul>
           <li><span>Spanish</span> (native)</li>
-          <li><span>English</span> (advanced)
+          <li><span>English</span> (fluent)
             <p>IELTS Advanced English Certificate (2023)</p>
           </li>
         </ul>
