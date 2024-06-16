31be5122aa6572579daca1fa4c02c255806f0d1f
Renzo Demarco <renzo11demarco@gmail.com>
2024-06-15 22:33:14 -0300

    add navItems

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,18 +2,10 @@ import { useDispatch, useSelector } from 'react-redux';
 import { setActiveComponent } from '../../redux/actions';
 import './Navbar.css'
 
-const Navbar = () => {
+const Navbar = ({ navItems }) => {
 
   const dispatch = useDispatch();
   const activeComponent = useSelector(state => state.activeComponent)
-  const navItems = [
-    { name: 'Home', label: 'WELCOME!' },
-    { name: 'AboutMe', label: 'ABOUT ME' },
-    { name: 'Education', label: 'EDUCATION' },
-    { name: 'WorkExperience', label: 'WORK EXPERIENCE' },
-    { name: 'Projects', label: 'PROJECTS' },
-    { name: 'Contact', label: 'CONTACT' }
-  ];
 
   const handleClick = (component) => {
     dispatch(setActiveComponent(component));
--- src/components/NavbarContainer/NavbarContainer.jsx
+++ src/components/NavbarContainer/NavbarContainer.jsx
@@ -3,12 +3,22 @@ import img from '../../assets/profile.webp'
 import Navbar from '../Navbar/Navbar'
 
 const NavbarContainer = () => {
+
+  const navItems = [
+    { name: 'Home', label: 'WELCOME!' },
+    { name: 'AboutMe', label: 'ABOUT ME' },
+    { name: 'Education', label: 'EDUCATION' },
+    { name: 'WorkExperience', label: 'WORK EXPERIENCE' },
+    { name: 'Projects', label: 'PROJECTS' },
+    { name: 'Contact', label: 'CONTACT' }
+  ];
+
   return (
     <div className='navbar-container'>
       <div className='img-container'>
         <img src={img} alt='Renzo Demarco' className='navbar-profile-img' />
       </div>
-      <Navbar />
+      <Navbar navItems={navItems}/>
     </div>
   )
 }
