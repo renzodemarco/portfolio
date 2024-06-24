ef48f1466e7d4d7c50322f06b1bcaf82c7504f01
Renzo Demarco <renzo11demarco@gmail.com>
2024-06-23 22:51:32 -0300

    add onClick in MailButton

--- src/components/MailButton/MailButton.jsx
+++ src/components/MailButton/MailButton.jsx
@@ -1,11 +1,20 @@
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 import { TbCopy } from "react-icons/tb";
 import './MailButton.css'
+import { useState } from 'react';
 
 const MailButton = () => {
+
+  const [isCopied, setIsCopied] = useState(false)
+
+  const copyMail = () => {
+    setIsCopied(true)
+    setTimeout(() => setIsCopied(false), 2000)
+  }
+
   return (
-    <CopyToClipboard text='renzo11demarco@gmail.com' onCopy={() => alert('Email copied')}>
-      <button className='copy-button-container'>
+    <CopyToClipboard text='renzo11demarco@gmail.com' onCopy={copyMail}>
+      <button className={`copy-button-container ${isCopied ? 'copied' : ''}`}>
         <div className='copy-button'>
           <p>renzo11demarco@gmail.com</p>
           <TbCopy className='copy-icon' />
