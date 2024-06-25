58f900ebd0e9a811c1547516812d6ba149e1051e
Renzo Demarco <renzo11demarco@gmail.com>
2024-06-25 20:15:36 -0300

    fix language button

--- src/components/LanguageButton/LanguageButton.jsx
+++ src/components/LanguageButton/LanguageButton.jsx
@@ -1,19 +1,32 @@
 import './LanguageButton.css'
 import i18n from '../../i18n.js'
+import { useState, useEffect } from 'react';
 
 const LanguageButton = () => {
 
-  const current = i18n.language;
+  const [currentLanguage, setCurrentLanguage] = useState(i18n.language);
 
+  useEffect(() => {
+    const handleLanguageChange = (lng) => {
+      setCurrentLanguage(lng);
+    };
+
+    i18n.on('languageChanged', handleLanguageChange);
+
+    return () => {
+      i18n.off('languageChanged', handleLanguageChange);
+    };
+  }, []);
+  
   const changeLanguage = () => {
-    if (current === 'es') i18n.changeLanguage('en')
+    if (currentLanguage === 'es') i18n.changeLanguage('en')
     else i18n.changeLanguage('es')
   };
 
   return (
     <div className="lang-btn-container">
       <button className="lang-btn" onClick={changeLanguage}>
-        {current === 'es' ? 'EN' : 'ES'}
+        {currentLanguage === 'es' ? 'EN' : 'ES'}
       </button>
     </div>
   )
